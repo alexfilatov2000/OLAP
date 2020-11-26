fb9c69b38053b0e90ac10aef464bec0c3dcf7e47
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-11-26 18:29:53 +0200

    Little changes in code.

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
+import _ from 'lodash';
 import Koa from 'koa';
 import Router from 'koa-router';
 import parse from "csv-parser";
 import fs from 'fs';
-const app = new Koa();
-const router = new Router();
 import dotenv from 'dotenv';
-dotenv.config();
 import pg from 'pg';
 
+const app = new Koa();
+const router = new Router();
+
+dotenv.config();
+
 //OLAPstage
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-});
+// const pool = new pg.Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+// });
 
+const getAnimeInsertQuery = (values) => 
+    `INSERT INTO anime (name, genre, type, episodes, rating, viewers) VALUES (${values.join(', ')})`;
 
-pool.connect();
+// pool.connect();
+
 app.use(router.routes());
+
 const PORT = 3000;
-router.get('/',async ctx =>{
+
+router.get('/', async ctx => {
     console.log(process.env.DB_USER);
-    let csvData=[];
+    
+    const data = [];
+    
     fs.createReadStream('csv/anime.csv')
-        .pipe(parse({delimiter: ','}))
-        .on('data', function(csvRow) {
-            //console.log(csvRow);
-
-            csvData.push(csvRow);
-
+        .pipe(parse({ delimiter: ',' }))
+        .on('data', (row) => {
+            data.push(_.pick(row, ['name', 'genre', 'type', 'episodes', 'rating', 'members']));
         })
-        .on('end',function() {
-            //do something with csvData
-            let result = csvData.map(anime => ({ name: anime.name, genre: anime.genre,
-                type: anime.type, episodes: anime.episodes,rating: anime.rating,members:anime.members}));
-            console.log(result);
+        .on('end', () => {
+            const queries = data.map(values => getInsertQuery([
+                `"${values.name}"`,
+                `"${values.genre}"`,
+                `"${values.type}"`,
+                +values.episodes,
+                +values.rating,
+                +values.members
+            ]));
+
+            console.dir(queries);
         });
-    await pool.query(`INSERT INTO anime (name,genre,type,episodes,rating,viewers)
-    VALUES ('name','genre','type',1,1,1)`);
 })
 
 app.listen(PORT, () => {
