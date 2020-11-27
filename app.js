642117f39f6de59adae8c4fa459f0316ff173f03
Alex Filatov <tanyaarni@gmail.com>
2020-11-27 10:53:36 +0200

    bug fixes

--- app.js
+++ app.js
@@ -3,55 +3,17 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import parse from 'csv-parser'
 import fs from 'fs';
-import dotenv from 'dotenv';
-import pg from "pg";
 const app = new Koa();
 const router = new Router();
+import pool from "./db/db";
+import {check, getAnimeInsertQuery} from "./checkErrors";
 
-dotenv.config();
 app.use(router.routes());
-
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-});
-pool.connect();
 
 const PORT = 3000;
 
-const getAnimeInsertQuery = (values) =>
-    `INSERT INTO anime (name, genre, type, episodes, rating, viewers) VALUES (${values.join(',')})`;
-
-
-const check = (queries) => {
-    let list = queries.split(/[,)]/);
-    let list2 = queries.split(',');
-    let episodes = +list[list.length - 4];
-    let rating = +list[list.length - 3];
-    let viewers = +list[list.length - 2];
-
-    if (isNaN(episodes) === true){
-        list2[list2.length - 3] = 0;
-        list2[list2.length - 3] = 0
-        const elem = `${list2.join()}`;
-        pool.query(elem);
-    }
-    if (isNaN(rating) === true){
-        list2[list2.length - 2] = 0;
-        const elem = `${list2.join()}`;
-        pool.query(elem);
-        console.log(elem);
-    }
-    if (isNaN(viewers) === true){
-        list2[list2.length - 1] = 0;
-        const elem = `${list2.join()})`;
-        pool.query(elem);
-        console.log(elem)
-    }
-}
-
 router.get('/', async ctx => {
+    await pool.query('TRUNCATE anime')
     const data = [];
 
     fs.createReadStream('csv/anime.csv')
@@ -79,7 +41,6 @@ router.get('/', async ctx => {
             }
             console.log(cnt);
         });
-
 
 })
 
--- db/db.js
+++ db/db.js
@@ -1,11 +1,13 @@
-// import pg from 'pg';
-//
-// // OLAPstage
-// const pool = new pg.Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_DATABASE,
-// });
-// pool.connect();
-//
-// export default pool
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// OLAPstage
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+});
+pool.connect();
+
+export default pool;
